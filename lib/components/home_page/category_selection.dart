5cfe429911c11a4d98670afb4f994cbbe5724a61
Codding BBQ <codingbbq@gmail.com>
2020-04-24 22:49:33 +0530

    Updated app to use the http provider

--- lib/components/home_page/category_selection.dart
+++ lib/components/home_page/category_selection.dart
@@ -25,47 +25,52 @@ class Category extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
+    return InkWell(
+      onTap: () {
+        print(name);
+      },
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
 
-          // Container widget to show the icon
-          Container(
-            width: 75,
-            height: 75,
-            margin: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: Colors.grey[200],
-                  offset: Offset(0.0, 5.0),
-                  blurRadius: 10.0,
-                  spreadRadius: 3.0
+            // Container widget to show the icon
+            Container(
+              width: 75,
+              height: 75,
+              margin: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.grey[200],
+                    offset: Offset(0.0, 5.0),
+                    blurRadius: 10.0,
+                    spreadRadius: 3.0
+                  )
+                ],
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  style: BorderStyle.none
                 )
-              ],
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                style: BorderStyle.none
-              )
+              ),
+              child: icon,
             ),
-            child: icon,
-          ),
 
-          SizedBox(height: 5.0,),
+            SizedBox(height: 5.0,),
 
-          // Show the Category text
-          Text(
-            name,
-            style: TextStyle(
-              color: secondaryTextColor,
-              fontSize: 14.0,
-              fontWeight: FontWeight.bold
-            ),
-          )
-        ],
-      )
+            // Show the Category text
+            Text(
+              name,
+              style: TextStyle(
+                color: secondaryTextColor,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
